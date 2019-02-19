2658877454d0c6da14ab6e0c4616323623bcb04b
hkhcc <29171587+hkhcc@users.noreply.github.com>
2019-02-19 10:23:08 +0800

    Add files via upload

--- build_bed_file.py
+++ build_bed_file.py
@@ -5,22 +5,35 @@ This script takes a list of transcript names and generates a sorted BED file.
 Usage:
     build_bed_file.py [TRANSCRIPT-NAME-1] [TRANSCRIPT-NAME-2]
 """
-import sys
+import argparse
 
 from autoprimer import Gene
 
-def generate_bed(transcripts):
+def generate_bed(transcripts, flanking=10):
     unsorted_output = list()
     for transcript in transcripts:
-        assert len(transcript.split('-')) == 2, transcript + ' is not supported!'
-        gene_name, transcript_number = transcript.split('-')
-        g = Gene(gene_name, version='GRCh37')
-        g.set_transcript(transcript)
+        if len(transcript.split('-')) == 2:
+            # user-specified transcript
+            gene_name, transcript_number = transcript.split('-')
+            g = Gene(gene_name, version='GRCh37')
+            g.set_transcript(transcript)
+        else:
+            # use canonical transcript from Ensembl
+            gene_name = transcript
+            g = Gene(gene_name, version='GRCh37')
+            g.set_transcript()
         exons = g.list_exon_regions()
         for i, exon in enumerate(exons, 1):
             cds = g.exon_to_translated(exon)
             if cds[1] is not None and cds[2] is not None:
-                unsorted_output.append(['chr' + str(cds[0]), cds[1], cds[2], gene_name.upper() + '.EX{exon:03}'.format(exon=i)])
+                assert cds[1] < cds[2], 'cds[1] is not less than cds[2]!'
+                flanked_cds = [cds[1] - flanking, cds[2] + flanking]
+                unsorted_output.append(['chr' + str(cds[0]),
+                                        flanked_cds[0], flanked_cds[1],
+                                        gene_name.upper() + '.EX{exon:03}'.format(exon=i) +
+                                        '.' + str(cds[1]) + '.' + str(cds[2])
+                                        ]
+                                        )
     return unsorted_output
 
 def sort_regions(region_list):
@@ -28,11 +41,12 @@ def sort_regions(region_list):
     return sorted_regions
 
 def main():
-    if len(sys.argv) < 2:
-        print('Usage: build_bed_file.py [TRANSCRIPT-NAME-1]...')
-        sys.exit()
+    parser = argparse.ArgumentParser(description='Builds a BED file from a list of provided genes/ transcripts.')
+    parser.add_argument('transcripts', nargs='+', help='gene name or trancript, e.g. ATP7B or ATP7B-201')
+    parser.add_argument('-f', dest='cds_flank', type=int, default=10, help='flanking region to add to each target interval (default: 10)')
+    args = parser.parse_args()
     # pass the command line arguments to the generate_bed() function
-    unsorted_output = generate_bed(sys.argv[1:])
+    unsorted_output = generate_bed(args.transcripts, flanking=args.cds_flank)
     sorted_output = sort_regions(unsorted_output)
     for line in sorted_output:
         print('\t'.join([str(x) for x in line]))
